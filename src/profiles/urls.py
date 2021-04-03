09f0db9003833b6fbe8f77b58b6f206161a2886d
aucoeur <aucoeur.ngo@students.makeschool.com>
2021-04-02 23:00:24 -0700

    main profile llistview and userview

--- src/profiles/urls.py
+++ src/profiles/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import profile_user_view
+from .views import profile_main_view, profile_user_view
 
 urlpatterns = [
-    path('', profile_user_view, name='profile')
+    path('', profile_main_view, name='profile-list-view'),
+    path('<int:id>', profile_user_view, name='user-view'),
 ]
--- src/profiles/views.py
+++ src/profiles/views.py
@@ -1,6 +1,15 @@
+from django.core.serializers import serialize
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from django.contrib.auth.models import User
+from .models import Profile
+
 # Create your views here.
+def profile_main_view(*args, **kwargs):
+    users = Profile.objects.all()
+
+    return HttpResponse(f'<h1>User Profile</h1><div>{users}</div>')
+
 def profile_user_view(*args, **kwargs):
-    return HttpResponse('<h1>User Profile</h1>')
+    return HttpResponse(Profile.objects.get(id=kwargs['id']))
